2e2b46cf2d735583a48593b16136244a0a2af888
ZAMZA <salimzamza@gmail.com>
2025-04-08 02:16:14 +0700

    Update App.tsx

--- examples/frontend/src/App.tsx
+++ examples/frontend/src/App.tsx
@@ -81,11 +81,19 @@ function App() {
 <Card style={{ marginBottom: '2rem' }}>
   <div style={{ textAlign: 'center' }}>
     <h2><b>Community Team</b></h2>
-    <p>
-  <a href="https://www.airdropasc.com" target="_blank"><img src="https://github.com/user-attachments/assets/1aa6d82b-3b3c-4ecb-93f9-e6a29f5826ca" width="50"/></a>&nbsp;&nbsp;&nbsp;
-  <a href="https://t.me/airdropasc" target="_blank"><img src="https://github.com/user-attachments/assets/56e7f6ee-18b7-4b36-becc-ec6e4de7bff9" width="50"/></a>&nbsp;&nbsp;&nbsp;
-  <a href="https://x.com/Autosultan_team" target="_blank"><img src="https://github.com/user-attachments/assets/fbb43aa4-9652-4a49-b984-5cf032b6b1ac" width="50"/></a>&nbsp;&nbsp;&nbsp;
-  <a href="https://www.youtube.com/@ZamzaSalim" target="_blank"><img src="https://github.com/user-attachments/assets/c15509f9-acb7-49ce-989a-5bac62e7e549" width="50"/></a>
+    <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <a href="https://www.airdropasc.com" target="_blank" style={{ margin: '0 10px' }}>
+        <img src="https://github.com/user-attachments/assets/1aa6d82b-3b3c-4ecb-93f9-e6a29f5826ca" width="50" />
+      </a>
+      <a href="https://t.me/airdropasc" target="_blank" style={{ margin: '0 10px' }}>
+        <img src="https://github.com/user-attachments/assets/56e7f6ee-18b7-4b36-becc-ec6e4de7bff9" width="50" />
+      </a>
+      <a href="https://x.com/Autosultan_team" target="_blank" style={{ margin: '0 10px' }}>
+        <img src="https://github.com/user-attachments/assets/fbb43aa4-9652-4a49-b984-5cf032b6b1ac" width="50" />
+      </a>
+      <a href="https://www.youtube.com/@ZamzaSalim" target="_blank" style={{ margin: '0 10px' }}>
+        <img src="https://github.com/user-attachments/assets/c15509f9-acb7-49ce-989a-5bac62e7e549" width="50" />
+      </a>
     </p>
   </div>
 </Card>
